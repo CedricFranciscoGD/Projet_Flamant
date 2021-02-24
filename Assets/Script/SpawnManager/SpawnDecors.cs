3527bc5696f6d45bc2b11ccf715aeab1e357939f
RomainP577 <60214885+RomainP577@users.noreply.github.com>
2021-02-24 13:21:50 +0100

    v1 fonctionnelle

---
+++ Assets/Script/SpawnManager/SpawnDecors.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDecors : MonoBehaviour
+{
+    [SerializeField] GameObject m_decors;
+
+    [SerializeField] private float m_riveGaucheX = 10;
+    [SerializeField] private float m_riveDroiteX = 1;
+    [SerializeField] private float m_spawnHeight = 10;
+    
+    [SerializeField] private float m_spawnPosZ;
+    
+    [SerializeField] private float m_prefabLenghtX;
+
+    [SerializeField] private int m_maxInstances = 50;
+    private int m_countInstances = 0;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        while (m_countInstances < m_maxInstances)
+        {
+            GenerateObstacle();
+            m_countInstances++;
+        }
+    }
+
+    void GenerateObstacle()
+    {
+        Instantiate(m_decors, new Vector3(m_riveGaucheX, m_spawnHeight, m_spawnPosZ),m_decors.transform.rotation);
+        Instantiate(m_decors, new Vector3(m_riveDroiteX, m_spawnHeight, m_spawnPosZ),m_decors.transform.rotation);
+        IncrementSpawnZ();
+    }
+
+    void IncrementSpawnZ()
+    {
+        m_spawnPosZ -= m_prefabLenghtX;
+    }
+}
