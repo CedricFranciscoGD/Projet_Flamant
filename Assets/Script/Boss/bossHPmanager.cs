03e6826043db2ee8f97a31fa2c4b9caf224f490d
CedricFranciscoGD <59953565+CedricFranciscoGD@users.noreply.github.com>
2021-03-03 14:48:42 +0100

    Kill Boss
    Le boss n'entre en contact plus qu'avec les munitions du joueur.

--- Assets/Script/Boss/bossHPmanager.cs
+++ Assets/Script/Boss/bossHPmanager.cs
@@ -2,33 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bossHPmanager : MonoBehaviour
+public class bossHpManager : MonoBehaviour
 {
     //vars
     [SerializeField] public float m_bossHP;
     [SerializeField] public float m_damageAmmo;
 
-    // Update is called once per frame
+    //* Update is called once per frame
+    
     void Update()
     {
-        //win function trigger when boss die
-        if (m_bossHP <= 0)
-        {
-            KillBoss();
-        }
+        
     }
     
     //Collide function with ammos
+    //If boss get hit from player ammos 
+    //Lose hp 
+    //If Boss HP = 0 boss die and launch win fonction
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(message:"AmmoHit");
-        Debug.Log(m_bossHP);
-        m_bossHP -= m_damageAmmo;
+        if (other.CompareTag("PlayerShoot"))
+            {
+                m_bossHP -= m_damageAmmo;
+                Debug.Log(message:"AmmoHit");
+                Debug.Log(m_bossHP);
+            }
+    //win function trigger when boss die
+        if (m_bossHP <= 0)
+            {
+                KillBoss();
+            }
     }
 
     //win function for boss kill
     void KillBoss()
     {
         Debug.Log("BossKilled");
+        Destroy(gameObject);
     }
 }
