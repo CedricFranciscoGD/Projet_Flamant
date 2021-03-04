a2435235086262def3516b1649e71478ab2f0fee
RomainP577 <60214885+RomainP577@users.noreply.github.com>
2021-03-04 15:00:45 +0100

    tweak

--- Assets/Script/Boss/bossHPmanager.cs
+++ Assets/Script/Boss/bossHPmanager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BossHpManager : MonoBehaviour
+public class bossHPmanager : MonoBehaviour
 {
     
     //vars
