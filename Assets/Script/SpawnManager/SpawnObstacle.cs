5cb57306d5b8b441471c9614d9855c8b64464a66
RomainP577 <60214885+RomainP577@users.noreply.github.com>
2021-03-06 15:29:39 +0100

    rocks,, tweak variables and improve

--- Assets/Script/SpawnManager/SpawnObstacle.cs
+++ Assets/Script/SpawnManager/SpawnObstacle.cs
@@ -8,7 +8,9 @@ using Random=UnityEngine.Random;
 public class SpawnObstacle : MonoBehaviour
 {
     
-    [SerializeField] GameObject m_obstacle;
+    [SerializeField] GameObject m_rockObstacle1;
+    [SerializeField] GameObject m_rockObstacle2;
+    [SerializeField] GameObject m_rockObstacle3;
 
     [SerializeField] private float m_maxSpawnX = 10;
     [SerializeField] private float m_minSpawnX = 1;
@@ -21,20 +23,33 @@ public class SpawnObstacle : MonoBehaviour
     
     [SerializeField] private int m_maxInstances = 50;
     private int m_countInstances = 0;
+    private int m_pickRock = 0;
     
     // Update is called once per frame
     void Update()
     {
         while (m_countInstances < m_maxInstances)
         {
+            m_pickRock = Random.Range(1, 4);
             GenerateObstacle();
             m_countInstances++;
         }
     }
 
     void GenerateObstacle()
-    {
-        Instantiate(m_obstacle, new Vector3(Random.Range(m_minSpawnX, m_maxSpawnX), m_spawnHeight, m_spawnPosZ),m_obstacle.transform.rotation);
+    { 
+        if (m_pickRock == 2)
+        {
+            Instantiate(m_rockObstacle1, new Vector3(Random.Range(m_minSpawnX, m_maxSpawnX), m_spawnHeight, m_spawnPosZ),m_rockObstacle1.transform.rotation);
+        }
+        else if (m_pickRock < 2)
+        {
+            Instantiate(m_rockObstacle2, new Vector3(Random.Range(m_minSpawnX, m_maxSpawnX), m_spawnHeight, m_spawnPosZ),m_rockObstacle2.transform.rotation);
+        }
+        else
+        {
+            Instantiate(m_rockObstacle3, new Vector3(Random.Range(m_minSpawnX, m_maxSpawnX), m_spawnHeight, m_spawnPosZ),m_rockObstacle3.transform.rotation);
+        }
         IncrementSpawnZ();
     }
 
