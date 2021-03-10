54f574f471d715d8d80771618ae7e83e9af13af1
CedricFranciscoGD <franciscomca@live.fr>
2021-03-10 22:20:21 +0100

    Audio
    Ajout piste audio
    Delay de mort

--- Assets/Script/GameManager/CollideRetry.cs
+++ Assets/Script/GameManager/CollideRetry.cs
@@ -10,6 +10,28 @@ public class CollideRetry : MonoBehaviour
     [SerializeField] private float m_yPos;//position axe Y
     [SerializeField] private float m_zPos;//position axe Z
     
+    
+    [SerializeField] private float m_elapsedTime;
+    [SerializeField] private float m_delay;
+
+    public AudioSource playerDead;
+
+    public bool isDead = false;
+
+
+    private void Update()
+    {
+        if (isDead)
+        {
+            m_elapsedTime += Time.deltaTime;
+            
+            if (m_elapsedTime >= m_delay)
+            {
+                SceneManager.LoadScene("Game_Over_Menu");
+            }
+            
+        }
+    }
 
     /// <summary>
     /// Si le joueur rentre en contact avec un danger, la fonction "EndGame" du GameManager est lancée
@@ -19,14 +41,15 @@ public class CollideRetry : MonoBehaviour
     {
         if (other.CompareTag("Danger"))
         {
+            playerDead.Play();
             //Debug.Log(message:"Danger");
-            SceneManager.LoadScene("Game_Over_Menu");
+            isDead = true;
         }
         
         else if (other.CompareTag("Enemy"))
         {
-            //Debug.Log(message:"Enemy");
-            SceneManager.LoadScene("Game_Over_Menu");
+            playerDead.Play();
+            isDead = true;
         }
         
     }
