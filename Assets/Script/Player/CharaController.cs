a9586b4c287a2be799a588d209a8d2d2383281d6
CedricFranciscoGD <franciscomca@live.fr>
2021-03-11 01:27:04 +0100

    V1

--- Assets/Script/Player/CharaController.cs
+++ Assets/Script/Player/CharaController.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharaController : MonoBehaviour
 {
-    [SerializeField] private float m_speed;//character speed
-    [SerializeField] private float m_leftBand;//left border limit
-    [SerializeField] private float m_rightBand;//right border limit
+    [SerializeField] private float m_speed; //character speed
+    [SerializeField] private float m_leftBand; //left border limit
+    [SerializeField] private float m_rightBand; //right border limit
 
     public bool playerIsDead;
 
@@ -17,41 +18,60 @@ public class CharaController : MonoBehaviour
     [SerializeField] private float m_increaseSpeed = 1.1f;
     [SerializeField] private float m_posZ;
     
-    private void Start()
-    {
-        playerIsDead = GetComponent<CollideRetry>().isDead;
-    }
+    [SerializeField] private float m_xPos;//position axe X
+    [SerializeField] private float m_yPos;//position axe Y
+    [SerializeField] private float m_zPos;//position axe Z
 
+    [SerializeField] private float m_elapsedTime;
+    [SerializeField] private float m_delay;
+
+    public bool isDead = false;
+
+    private float m_leftDir = 0.1f;
+    private float m_rightDir = 0.1f;
+    private float m_angle = 90;
+
+    public AudioSource playerDead;
 
     // Update is called once per frame
     void Update()
     {
         m_posZ = transform.position.z;
-        Debug.Log("Player "+ m_posZ);
-        // récupère la valeur de l'axe
-        float inputValue = Input.GetAxis("Horizontal");
+        Debug.Log("Player " + m_posZ);
 
-        // deplace le personnage
-        transform.Translate(Vector3.left * m_speed * Time.deltaTime * inputValue);
-        //Déplace le personnage automatiquement
-        transform.Translate(Vector3.back * m_speed * Time.deltaTime);
+        if (!isDead)
+        {
+            // récupère la valeur de l'axe
+            float inputValue = Input.GetAxis("Horizontal");
+
+            // deplace le personnage
+            transform.Translate(Vector3.left * m_speed * Time.deltaTime * inputValue);
+            //Déplace le personnage automatiquement
+            transform.Translate(Vector3.back * m_speed * Time.deltaTime);
+        }
+
+        if (isDead)
+        {
+            m_elapsedTime += Time.deltaTime;
+
+            if (m_elapsedTime >= m_delay)
+            {
+                SceneManager.LoadScene("Game_Over_Menu");
+            }
+
+        }
 
         // delimite la taille des déplacements
-        if(transform.position.x > m_leftBand)//left
+        if (transform.position.x > m_leftBand) //left
         {
             transform.position = new Vector3(m_leftBand, transform.position.y, transform.position.z);
         }
 
-        if(transform.position.x < m_rightBand)//right
+        if (transform.position.x < m_rightBand) //right
         {
             transform.position = new Vector3(m_rightBand, transform.position.y, transform.position.z);
         }
 
-        if (playerIsDead)
-        {
-            m_speed = 0;
-        }
-        
         //SpeedIncrease
         m_countDistance = Math.Abs(transform.position.z);
         if (m_countDistance > m_countMax)
@@ -64,6 +84,37 @@ public class CharaController : MonoBehaviour
     {
         m_speed = m_speed * m_increaseSpeed;
         m_countMax = m_countMax + m_countMaxAdd;
+    }
+
+    /// <summary>
+    /// Si le joueur rentre en contact avec un danger, la fonction "EndGame" du GameManager est lancée
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider p_other)
+    {
+        if (!isDead)
+        {
+            if (p_other.CompareTag("Danger"))
+            {
+                playerDead.Play();
+                //Debug.Log(message:"Danger");
+                isDead = true;
+                DeadPos();
+            }
+
+            else if (p_other.CompareTag("Enemy"))
+            {
+                playerDead.Play();
+                isDead = true;
+                DeadPos();
+            }
+        }
+    }
+    
+    private void DeadPos()
+    {
+        transform.Rotate(Vector3.back * m_angle);
+        transform.Translate(0, -1, 0);
     }
 }
 
